d0e4679ec45bf56ccd99fc97377beabc2120f35d
George Higbie <gehigbie@gmail.com>
2017-01-15 19:22:10 -0700

    added function call

--- js.js
+++ js.js
@@ -5,4 +5,9 @@ var obj = {
 	age: 45,
 	isCool: false,
 	friends: ["Peter", "Sammy", "Tabby"],
-}
+	add: function(x,y){  //added a function and setting it as a value for the property add
+		return x + y;
+	}
+};
+obj.add(20,3); //this looks exactly like the console.log function
+console.log(obj.add(10.5));
